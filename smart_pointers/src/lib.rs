42c34377fb9c02c55999c758539aa5c5672bf6e6
Bruno Paulino <bruno.paulino@n26.com>
2022-12-04 19:58:55 +0100

    Implement Deref

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -1,5 +1,23 @@
+use std::ops::Deref;
+
 #[derive(Debug)]
 pub enum List {
-  Cons(i32, Box<List>),
-  Nil
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+pub struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,16 +1,22 @@
 use smart_pointers::List::{Cons, Nil};
+use smart_pointers::MyBox;
+
+fn hello(name: &str) {
+    println!("Hello {}", name);
+}
 
 fn main() {
-  let list = Cons(
-    1, 
-    Box::new(Cons(
-      2, 
-      Box::new(Cons(
-          3, 
-          Box::new(Nil))
-        )
-      )
-    )
-  );
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("Cons list: {:?}", list);
+
+    // playing with my own smart pointer
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // Deref works on MyBox with other times that implement Deref
+    let name = MyBox::new(String::from("Bruno"));
+    hello(&name);
 }
