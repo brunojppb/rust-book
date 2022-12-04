9dbe3110873521347a1ad94f58727ad0eb79368c
Bruno Paulino <bruno.paulino@n26.com>
2022-12-04 19:10:31 +0100

    Using box type for recursive data structures

---
+++ smart_pointers/src/main.rs
@@ -0,0 +1,16 @@
+use smart_pointers::List::{Cons, Nil};
+
+fn main() {
+  let list = Cons(
+    1, 
+    Box::new(Cons(
+      2, 
+      Box::new(Cons(
+          3, 
+          Box::new(Nil))
+        )
+      )
+    )
+  );
+    println!("Cons list: {:?}", list);
+}
