41c3517610669ad3a8794f5fbd3bb59075789b0a
Bruno Paulino <bruno@bpaulino.com>
2022-12-07 22:05:57 +0100

    Multiple producers and one receiver

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -2,7 +2,45 @@ use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
     // send_once();
-    send_multiple();
+    // send_multiple();
+    multiple_producers();
+}
+
+fn multiple_producers() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let values = vec![
+            String::from("hi"),
+            String::from("there"),
+            String::from("from"),
+            String::from("thread 1"),
+        ];
+
+        for val in values {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let values = vec![
+            String::from("this"),
+            String::from("is"),
+            String::from("now"),
+            String::from("thread 2"),
+        ];
+
+        for val in values {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got message: {}", received);
+    }
 }
 
 fn send_multiple() {
