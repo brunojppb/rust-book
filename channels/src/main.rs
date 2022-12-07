124053b79fb758c7e78745cd1e50773d68e4f232
Bruno Paulino <bruno@bpaulino.com>
2022-12-07 22:02:53 +0100

    Sending multiple messages from producer

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -1,6 +1,32 @@
-use std::{sync::mpsc, thread};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
+    // send_once();
+    send_multiple();
+}
+
+fn send_multiple() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let values = vec![
+            String::from("hi"),
+            String::from("there"),
+            String::from("from"),
+            String::from("thread 1"),
+        ];
+
+        for val in values {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got message: {}", received);
+    }
+}
+
+fn send_once() {
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
